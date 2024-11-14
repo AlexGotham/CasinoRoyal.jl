fd5d6ae0437161d58d73ebc54bbcc1b6dd6229de
AlexGotham <aleger567@gmail.com>
2024-11-14 09:00:45 +0100

    Update CasinoRoyal.jl

--- src/CasinoRoyal.jl
+++ src/CasinoRoyal.jl
@@ -3,8 +3,7 @@ module CasinoRoyal
     # Write your package code here.
     using Vizagrams
     using Random
-    using Plots
-    export rouedessin, rouenombre, capture_svg
+    export rouedessin, rouenombre
 
     include("roue.jl")
 
